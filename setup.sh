b56c1562e7fdef0831224d9d59c810ba7fcbbc17
Elihuso Quigley <Elihuso@outlook.com>
2024-09-05 10:28:20 +0800

    [F] Use path instead username

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-sed -i ./Big_Sur/Big_Sur.xml -e "s/elihuso/$USER/g"
-sed -i ./Catalina/Catalina.bak.xml -e "s/elihuso/$USER/g"
-sed -i ./mojave_dynamic/mojave_dynamic.xml -e "s/elihuso/$USER/g"
-sed -i ./Ventura_Abstract/Ventura_Abstract.xml -e "s/elihuso/$USER/g"
+O=$(echo "$PWD" | sed -e 's/\//\\\//g')
+
+echo $O
+
+sed -i ./Big_Sur/Big_Sur.xml -e "s/\\/home\\/elihuso\\/Pictures\\/Wallpapers/$O/g"
+sed -i ./Catalina/Catalina.bak.xml -e "s/\\/home\\/elihuso\\/Pictures\\/Wallpapers/$O/g"
+sed -i ./mojave_dynamic/mojave_dynamic.xml -e "s/\\/home\\/elihuso\\/Pictures\\/Wallpapers/$O/g"
+sed -i ./Ventura_Abstract/Ventura_Abstract.xml -e "s/\\/home\\/elihuso\\/Pictures\\/Wallpapers/$O/g"
 
